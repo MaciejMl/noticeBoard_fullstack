2488be5bfff7c1a3c799fc7202bdd701843c81e9
MaciejMl <szypki@gmail.com>
2023-08-05 14:36:38 +0200

    Modify gitignore

--- db.js
+++ db.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const NODE_ENV = process.env.NODE_ENV;
 let dbURI = '';
 
-if (NODE_ENV === 'production') {
+if (NODE_ENV === 'production')
   dbURI = `mongodb+srv://szypki:${mySecret}@mytestingdb.unpaqol.mongodb.net/NoticeBoardDB?retryWrites=true&w=majority`;
-} else {
-  dbURI = 'mongodb://localhost:27017/NoticeBoardDB';
-}
+else dbURI = 'mongodb://localhost:27017/NoticeBoardDB';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
